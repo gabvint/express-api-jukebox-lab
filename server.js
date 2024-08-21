ea72e8aa05eed150f302d876f11c89cb8be2bb5c
gabtongol <geybtongol@gmail.com>
2024-08-20 21:31:55 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const methodOverride = require('method-override')
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const jukeboxRouter = require('./controllers/jukebox.js');
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// middlewares
+app.use(express.json());
+app.use(methodOverride('_method'))
+
+// Routes go here
+app.use('/jukebox', jukeboxRouter)
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
